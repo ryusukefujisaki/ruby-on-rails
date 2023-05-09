20f38b39c0a5d11c65daa8993201e4ab99d20cae
r_fujisaki <ryusukefujisaki@gmail.com>
2023-05-09 19:57:47 +0900

    Download foundation-sites.js

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,14 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+// custom
+import jquery from "jquery"
+import "foundation-sites"
+
+window.jQuery = jquery
+window.$ = jquery
+
+$(function() {
+  $(document).foundation();
+})
