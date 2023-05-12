c5dcb2bac17238405a12d9b8d896ce93352bd1eb
r_fujisaki <ryusukefujisaki@gmail.com>
2023-05-12 22:30:57 +0900

    Rename "welcome" to "framework"

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  root 'welcome#index'
+  root to: redirect('/bands')
 
   resources :bands
+
+  get '/framework', to: 'framework#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
