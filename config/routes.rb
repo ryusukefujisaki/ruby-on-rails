6a281affa0a2b19390e2344823efb4d6c212c0f5
r_fujisaki <ryusukefujisaki@gmail.com>
2023-05-29 21:37:43 +0900

    Add disk create page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   root to: redirect('/bands')
 
-  resources :bands
+  resources :bands do
+    resources :disks, only: [:new, :create]
+  end
 
   get '/framework', to: 'framework#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
